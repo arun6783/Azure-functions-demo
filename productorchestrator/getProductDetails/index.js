15d2ee8d7ef16f04617514354b12a179646def3a
Arunkumar Narayanaswamy <arunkmr1983@gmail.com>
2022-07-04 16:35:33 +0100

    test one

--- productorchestrator/getProductDetails/index.js
+++ productorchestrator/getProductDetails/index.js
@@ -3,7 +3,7 @@ module.exports = async function (context, req) {
 
   const name = req.query.name || (req.body && req.body.name)
   const responseMessage = name
-    ? 'Greetings!! with new github actions, ' +
+    ? 'hello world. welcome to my function, ' +
       name +
       '. This HTTP triggered function executed successfully.'
     : 'This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.'
