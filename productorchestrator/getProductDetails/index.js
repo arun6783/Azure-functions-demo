e18eea2b125687663e19200a9d4b254b32460d70
Arunkumar Narayanaswamy <arunkmr1983@gmail.com>
2022-07-05 11:38:34 +0100

    adding debug code

--- productorchestrator/getProductDetails/index.js
+++ productorchestrator/getProductDetails/index.js
@@ -3,19 +3,24 @@ module.exports = async function (context, req) {
   context.log('JavaScript HTTP trigger function processed a request.')
 
   const id = req.query.id
+  console.log('id is ', id)
+
   if (id) {
-    let result = []
+    try {
+      let result = []
 
-    const datas = await Promise.all(
-      getProductDetail(id),
-      getReviews(id),
-      getStock(id)
-    )
+      const datas = await Promise.all(
+        getProductDetail(id),
+        getReviews(id),
+        getStock(id)
+      )
 
-    datas.forEach(({ data }) => {
-      result = [...result, data]
-    })
-
+      datas.forEach(({ data }) => {
+        result = [...result, data]
+      })
+    } catch (e) {
+      console.log('errr', e)
+    }
     context.res = {
       // status: 200, /* Defaults to 200 */
       body: result,
@@ -28,6 +33,7 @@ module.exports = async function (context, req) {
 }
 
 const getProductDetail = (id) => {
+  console.log('product detail uri', process.env.ProductDetailUrl)
   return axios.get(
     `http://${process.env.ProductDetailUrl}/api/productdetail/${id}`
   )
