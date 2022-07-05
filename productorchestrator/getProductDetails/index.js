d92e5076f5407940d3171bbf684e1d4f1bb08d2f
Arunkumar Narayanaswamy <arunkmr1983@gmail.com>
2022-07-05 11:25:12 +0100

    adding endpoints to func app

--- productorchestrator/getProductDetails/index.js
+++ productorchestrator/getProductDetails/index.js
@@ -1,15 +1,42 @@
+const axios = require('axios').default
 module.exports = async function (context, req) {
   context.log('JavaScript HTTP trigger function processed a request.')
 
-  const name = req.query.name || (req.body && req.body.name)
-  const responseMessage = name
-    ? 'hello world. welcome to my function, ' +
-      name +
-      '. This HTTP triggered function executed successfully.'
-    : 'This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.'
+  const id = req.query.id
+  if (id) {
+    let result = []
 
-  context.res = {
-    // status: 200, /* Defaults to 200 */
-    body: responseMessage,
+    const datas = await Promise.all(
+      getProductDetail(id),
+      getReviews(id),
+      getStock(id)
+    )
+
+    datas.forEach(({ data }) => {
+      result = [...result, data]
+    })
+
+    context.res = {
+      // status: 200, /* Defaults to 200 */
+      body: result,
+    }
   }
+  context.res = {
+    status: 400,
+    body: 'Bad request',
+  }
+}
+
+const getProductDetail = (id) => {
+  return axios.get(
+    `http://${process.env.ProductDetailUrl}/api/productdetail/${id}`
+  )
+}
+
+const getReviews = async (id) => {
+  return axios.get(`http://${process.env.ReviewsUrl}/api/reviews/${id}`)
+}
+
+const getStock = async (id) => {
+  return axios.get(`http://${process.env.StockUrl}/api/stock/${id}`)
 }
